85f4c6da30a5d147c77647d6557160d4dc09a2e2
kdj9878 <kdj9878@gmail.com>
2021-09-05 18:00:04 +0900

    modify cartPage.js

--- src/main/webapp/resources/js/cartPage.js
+++ src/main/webapp/resources/js/cartPage.js
@@ -31,18 +31,18 @@ $(document).ready(function(){
 	})
 	
 	//체크 박스 선택 시 해당 제품의 정보들이 checkList 배열에 담김
-	$('.listCheckbox').change(function(){
+	$('.listCheckbox').change(function(event){
 		var checkbox = $(this);
 		var storeName = $(this).parent().prev().text();
 		var productName = $(this).parent().parent().next().text();
 		var productPhoto = $(this).attr('value4');
 		var o_o_nop = $(this).attr('value5');
-		var sumPrice = $(this).parent().parent().parent().next().children('.sumPrice').val();
 		var quantideyBox = checkbox.parent().parent().parent().next().children('.quantidey');
 		var defaultprice = Number(checkbox.attr('defaultValue'));
 		var o_no = checkbox.attr('value1');
 		var o_quantidey = Number(checkbox.attr('value3'));
 		var sum = (defaultprice*o_quantidey);
+		
 		if(checkbox.is(":checked")){
 			data = {
 				s_name : storeName,
@@ -55,43 +55,58 @@ $(document).ready(function(){
 			}
 			checkList.push(data);
 			jsonObject.data = checkList;
-			priceArray.push(Number(sumPrice));
+			
+			$.each(checkbox, function(i, item){
+			
+			var price = {index : o_o_nop, value : $(item).attr('value2')}
+			priceArray.push(price);			
+			})
 			quantideyBox.attr('readonly', true);
+			console.log(jsonObject.data)
 		}
 		else{
+			
 			checkList.pop();
-			priceArray.pop();
+			for(i = 0; i < priceArray.length; i++){
+				if(priceArray[i].index == o_o_nop){
+					console.log(o_o_nop)
+					priceArray.splice(i, 1);
+					i--;
+				}
+			}
+			
 			quantideyBox.attr('readonly', false);
+			console.log(jsonObject.data)
 		}
+		
 		var sum = 0;
 		for(i=0;i<priceArray.length;i++){
-			sum +=  priceArray[i];
+			sum +=  Number(priceArray[i].value);
 		}
-		/*console.log(jsonObject);*/
 		$('#resultPrice').children().text(sum);
 	})
 	
-	$('.allCheckBox').click(function(){
+	$('.allCheckBox').click(function(event){
 		var checked = $('.allCheckBox').is(":checked");
 		//체크되면 true 체크 안되어 있으면 false가 담김
 		if(checked){
 			var inputValue = $('input:checkbox');
 			inputValue.prop("checked",true);
 			//모든 체크 박스들을 선택해서 담음
-			var sumPrice = $('.sumPrice');
 			
 			$.each(inputValue, function(index, item){
-				var value1 = $(item).attr('value1');
-				var value2 = $(item).attr('defaultValue');
-				var value3 = Number($(item).attr('value3'));
-				var o_o_nop = $(item).attr('value5');
-				var storeName = $(item).parent().prev().text();
-				var productName = $(item).parent().parent().next().text();
-				var productPhoto = $(item).attr('value4');
-				var sum = Number(value2)*Number(value3);
+				var value1 = $(item).attr('value1');//제품 번호
+				var value2 = $(item).attr('defaultValue');//제품 개당 가격
+				var value3 = $(item).attr('value3');//제품 수량
+				var o_o_nop = $(item).attr('value5');//주문 고유 PK
+				var storeName = $(item).parent().prev().text();//상점 이름
+				var productName = $(item).parent().parent().next().text();//제품 이름
+				var productPhoto = $(item).attr('value4');//제품 사진
+				var sum = Number(value2)*Number(value3);//총합 가격
 				
+				//value2 속성(개당 가격)에 총합 가격을 담음
 				$(value2).attr('value2', sum);
-				console.log(value2)
+							
 				
 					temp = {
 						s_name : storeName,
@@ -103,21 +118,31 @@ $(document).ready(function(){
 						o_o_nop : o_o_nop
 												
 					}
+					//checkList 배열에 각각의 상품을 오브젝트 형태로 push
 					checkList.push(temp);
-					priceArray.push(sum)
+					//priceArray 배열에 총합 가격을 push
+					var price = {index : o_o_nop, value : $(item).attr('value2')}
+					priceArray.push(price);
 				})
+				//배열의 첫 번째 값에 전체 선택 버튼 객체가 들어가 있으므로
+				//shift를 통해서 배열 첫 번째 값 제거
 				checkList.shift();
-				console.log(checkList);
 				priceArray.shift();
+				
+				//json객체에 checkList를 담음
 				jsonObject.data = checkList;
 				
+				//제품 선택 버튼을 클릭할 경우 수량을 바꾸지 못하게
+				//readonly 속성을 부여함
 				$('.quantidey').attr('readonly', true);
-				var sum = 0;
+				
+				var synthesisPrice = 0;
 				for(i=0; i<priceArray.length;i++){
-					sum += priceArray[i];
+					synthesisPrice += Number(priceArray[i].value);
 				}
-				$('#resultPrice').children().text(sum);
-
+				//총합 가격을 나타내는 부분
+				$('#resultPrice').children().text(synthesisPrice);
+				console.log(jsonObject)
 			}
 		else {
 			var inputValue = $('input:checkbox');
@@ -127,11 +152,9 @@ $(document).ready(function(){
 			jsonObject.data = {};
 			$('#resultPrice').children().empty();
 			$('.quantidey').attr('readonly', false);
-			console.log(jsonObject);
 		}
 		
 		
-		console.log(jsonObject);
 	})
 	
 	
